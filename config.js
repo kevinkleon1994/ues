58e327563e8ac6f9c29d3dec66c1c904e092e60e
Kevin Fernandes <kevinkleon1994@gmail.com>
2026-08-09 16:01:48 -0300

    Bump version numbers to 1.0.4

--- config.js
+++ config.js
@@ -4,6 +4,6 @@
  */
 window.UES_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzAHf3of3mZ_aFYB96n7Nohe5AvSN0Ke98kds8656mivgvtxM7i25ljJ0TpZK-Emii7/exec';
 
-window.UES_VERSION = "1.0.1";
+window.UES_VERSION = "1.0.4";
 
-window.UES_APP_VERSION = '1.0.1';
+window.UES_APP_VERSION = '1.0.4';
